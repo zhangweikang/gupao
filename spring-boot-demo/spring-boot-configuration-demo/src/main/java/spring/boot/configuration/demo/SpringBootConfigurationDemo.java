48cbb19f34816c8410e96e9ea42647be9f14c58f
zhangweikang <1114658003@qq.com>
2019-09-30 14:20:28 +0800

    commits spring-boot-demo 配置加载

---
+++ spring-boot-demo/spring-boot-configuration-demo/src/main/java/spring/boot/configuration/demo/SpringBootConfigurationDemo.java
@@ -0,0 +1,16 @@
+package spring.boot.configuration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangweikang
+ * @date 2019/9/26
+ */
+@SpringBootApplication
+public class SpringBootConfigurationDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigurationDemo.class,args);
+    }
+}
---
+++ spring-boot-demo/spring-boot-configuration-demo/src/main/java/spring/boot/configuration/demo/config/MyConfiguration.java
@@ -0,0 +1,11 @@
+package spring.boot.configuration.demo.config;
+
+/**
+ * @author zhangweikang
+ * @date 2019/9/27
+ */
+public class MyConfiguration {
+    static {
+        System.out.println(" import MyConfiguration ");
+    }
+}
